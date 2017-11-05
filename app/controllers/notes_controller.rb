be0eb8226cc6405ae16ff5e3b734a4ec04364660
DanielRusinov <daniel.rusinov00@gmail.com>
2017-11-05 09:53:38 -0800

    ithinkimdone

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -26,17 +26,17 @@ class NotesController < ApplicationController
   def create
     if request.content_type =~ /xml/
       params[:message] = Hash.from_xml(request.body.read)["message"]
-      note = Note.new(body: params[:message])
+      note = Note.create(body: params[:message])
       render xml:
       '<?xml version = "1.0" encoding = "UTF-8" standalone = "yes"?>' +
       '<url>' +
         notes_url + "/" + note.id.to_s +
       '</url>'
     elsif request.content_type =~ /json/
-      note = Note.new(body: params[:message])
+      note = Note.create(body: params[:message])
       render json: {url: notes_url + "/" + note.id.to_s}
     elsif request.content_type =~ /form/
-      @note = Note.new(body: params[:content])
+      @note = Note.new(note_params)
       if @note.save
         redirect_to notes_url + "/" + @note.id.to_s
       else
